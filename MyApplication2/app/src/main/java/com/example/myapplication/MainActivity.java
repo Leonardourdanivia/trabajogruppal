8247ba597ce9a8f1ed65b2c2aa4c7f8a92691f53
Leonardourdanivia <40493946+Leonardourdanivia@users.noreply.github.com>
2023-08-28 16:58:32 -0500

    Add files via upload

---
+++ MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView username =(TextView) findViewById(R.id.username);
+        TextView password = (TextView) findViewById(R.id.password);
+
+        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
+
+        //admin and admin
+
+        loginbtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v ){
+                if(username.getText().toString().toString().equals("admin") && password.getText().toString().equals("admin")){
+
+                    Toast.makeText( MainActivity.this, text)
+                }
+
+            }
+        }
+    }
+}
